ad207382f9f4e2320559e9a2260f9d126368c95e
zenin5479 <zenin5479@gmail.com>
2025-07-06 01:03:49 +0300

    Update 6.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -79,6 +79,9 @@ namespace Homework_4._8
                string res = "В массиве найдена строка " + (i + 1) + " с элементом, кратным " + multiple;
                //Console.WriteLine("В массиве найдена строка {0} с элементом, кратным {1}", i + 1, multiple);
                Console.WriteLine(res);
+
+               //File.AppendAllLines(Path.Combine(docPath, "WriteFile.txt"), lines);
+
             }
 
             Array.Clear(lines, 0, lines.Length);
