a0cbd6b4f2f5ff6500b958364b8028b0a2d9f681
zenin5479 <zenin5479@gmail.com>
2025-07-06 10:22:31 +0300

    Update 7.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -29,7 +29,6 @@ namespace Homework_4._8
          int multipleElement = ClassFor2DArray.MultipleElement();
 
          string pathOne = Path.GetFullPath(nameFileOne);
-
          int[,] sourceOne = ClassFor2DArray.EnterArrayInt(pathOne, nameFileOne);
          if (sourceOne.GetLength(0) == 0)
          {
