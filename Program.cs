de625314ff051d82bfc6314ff429869581c5b638
zenin5479 <zenin5479@gmail.com>
2025-07-06 11:05:54 +0300

    Update 8.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using LibraryFor2DArray;
 
 // Разработать программу для обработки двумерного массива
 // При выполнении задания необходимо разработать подпрограмму,
@@ -21,22 +22,22 @@ namespace Homework_4._8
          string nameFileOne = "a.txt";
          string nameFileTwo = "finish.txt";
 
-         int rowOne = LibraryFor2DArray.VariousMethods.SizeRow();
-         int columnOne = LibraryFor2DArray.VariousMethods.SizeColumn();
-         int multipleElement = LibraryFor2DArray.VariousMethods.MultipleElement();
+         int rowOne = VariousMethods.SizeRow();
+         int columnOne = VariousMethods.SizeColumn();
+         int multipleElement = VariousMethods.MultipleElement();
 
          string pathOne = Path.GetFullPath(nameFileOne);
-         int[,] sourceOne = LibraryFor2DArray.VariousMethods.EnterArrayInt(pathOne, nameFileOne);
+         int[,] sourceOne = VariousMethods.EnterArrayInt(pathOne, nameFileOne);
          if (sourceOne.GetLength(0) == 0)
          {
             Console.WriteLine("Файл {0} пуст", nameFileOne);
          }
          else
          {
-            int[,] inputArray = LibraryFor2DArray.VariousMethods.InputArrayInt(sourceOne, rowOne, columnOne);
+            int[,] inputArray = VariousMethods.InputArrayInt(sourceOne, rowOne, columnOne);
             string pathTwo = Path.GetFullPath(nameFileTwo);
             File.Create(pathTwo).Close();
-            LibraryFor2DArray.VariousMethods.SplittingLines(inputArray, multipleElement, nameFileTwo);
+            VariousMethods.SplittingLines(inputArray, multipleElement, nameFileTwo);
          }
 
          Console.ReadKey();
